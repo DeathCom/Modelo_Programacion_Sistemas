e0287d48491fee4b352bc7b13582113d72202bf6
Carlos Perez <31164007+DeathCom@users.noreply.github.com>
2019-05-26 16:51:00 -0600

    Update
    Avance Nimbus

---
+++ Nimbus/Nimbus_01P/Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace Nimbus_01P
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.principalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nuevoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarComoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.Button_Abrir = new System.Windows.Forms.ToolStripButton();
+            this.Button_Nuevo = new System.Windows.Forms.ToolStripButton();
+            this.Button_Guardar = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.Button_Lexico = new System.Windows.Forms.ToolStripButton();
+            this.Button_Sintactico = new System.Windows.Forms.ToolStripButton();
+            this.Button_Semantico = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.Button_Salir = new System.Windows.Forms.ToolStripButton();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.Codigo = new System.Windows.Forms.TabPage();
+            this.Panel_Codigo = new System.Windows.Forms.RichTextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.Codigo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.principalToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1111, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // principalToolStripMenuItem
+            // 
+            this.principalToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirToolStripMenuItem,
+            this.nuevoToolStripMenuItem,
+            this.guardarToolStripMenuItem,
+            this.guardarComoToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.principalToolStripMenuItem.Name = "principalToolStripMenuItem";
+            this.principalToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.principalToolStripMenuItem.Text = "Principal";
+            // 
+            // abrirToolStripMenuItem
+            // 
+            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.abrirToolStripMenuItem.Text = "Abrir";
+            this.abrirToolStripMenuItem.Click += new System.EventHandler(this.abrirToolStripMenuItem_Click);
+            // 
+            // nuevoToolStripMenuItem
+            // 
+            this.nuevoToolStripMenuItem.Name = "nuevoToolStripMenuItem";
+            this.nuevoToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.nuevoToolStripMenuItem.Text = "Nuevo";
+            this.nuevoToolStripMenuItem.Click += new System.EventHandler(this.nuevoToolStripMenuItem_Click);
+            // 
+            // guardarToolStripMenuItem
+            // 
+            this.guardarToolStripMenuItem.Name = "guardarToolStripMenuItem";
+            this.guardarToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.guardarToolStripMenuItem.Text = "Guardar";
+            this.guardarToolStripMenuItem.Click += new System.EventHandler(this.guardarToolStripMenuItem_Click);
+            // 
+            // guardarComoToolStripMenuItem
+            // 
+            this.guardarComoToolStripMenuItem.Name = "guardarComoToolStripMenuItem";
+            this.guardarComoToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.guardarComoToolStripMenuItem.Text = "Guardar Como";
+            this.guardarComoToolStripMenuItem.Click += new System.EventHandler(this.guardarComoToolStripMenuItem_Click);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Button_Abrir,
+            this.Button_Nuevo,
+            this.Button_Guardar,
+            this.toolStripSeparator1,
+            this.Button_Lexico,
+            this.Button_Sintactico,
+            this.Button_Semantico,
+            this.toolStripSeparator2,
+            this.Button_Salir});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1111, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // Button_Abrir
+            // 
+            this.Button_Abrir.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.Button_Abrir.Image = ((System.Drawing.Image)(resources.GetObject("Button_Abrir.Image")));
+            this.Button_Abrir.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Button_Abrir.Name = "Button_Abrir";
+            this.Button_Abrir.Size = new System.Drawing.Size(23, 22);
+            this.Button_Abrir.Text = "toolStripButton1";
+            this.Button_Abrir.Click += new System.EventHandler(this.Button_Abrir_Click);
+            // 
+            // Button_Nuevo
+            // 
+            this.Button_Nuevo.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.Button_Nuevo.Image = ((System.Drawing.Image)(resources.GetObject("Button_Nuevo.Image")));
+            this.Button_Nuevo.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Button_Nuevo.Name = "Button_Nuevo";
+            this.Button_Nuevo.Size = new System.Drawing.Size(23, 22);
+            this.Button_Nuevo.Text = "toolStripButton2";
+            this.Button_Nuevo.Click += new System.EventHandler(this.Button_Nuevo_Click);
+            // 
+            // Button_Guardar
+            // 
+            this.Button_Guardar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.Button_Guardar.Image = ((System.Drawing.Image)(resources.GetObject("Button_Guardar.Image")));
+            this.Button_Guardar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Button_Guardar.Name = "Button_Guardar";
+            this.Button_Guardar.Size = new System.Drawing.Size(23, 22);
+            this.Button_Guardar.Text = "toolStripButton3";
+            this.Button_Guardar.Click += new System.EventHandler(this.Button_Guardar_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // Button_Lexico
+            // 
+            this.Button_Lexico.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.Button_Lexico.Image = ((System.Drawing.Image)(resources.GetObject("Button_Lexico.Image")));
+            this.Button_Lexico.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Button_Lexico.Name = "Button_Lexico";
+            this.Button_Lexico.Size = new System.Drawing.Size(23, 22);
+            this.Button_Lexico.Text = "toolStripButton4";
+            // 
+            // Button_Sintactico
+            // 
+            this.Button_Sintactico.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.Button_Sintactico.Image = ((System.Drawing.Image)(resources.GetObject("Button_Sintactico.Image")));
+            this.Button_Sintactico.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Button_Sintactico.Name = "Button_Sintactico";
+            this.Button_Sintactico.Size = new System.Drawing.Size(23, 22);
+            this.Button_Sintactico.Text = "toolStripButton5";
+            // 
+            // Button_Semantico
+            // 
+            this.Button_Semantico.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.Button_Semantico.Image = ((System.Drawing.Image)(resources.GetObject("Button_Semantico.Image")));
+            this.Button_Semantico.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Button_Semantico.Name = "Button_Semantico";
+            this.Button_Semantico.Size = new System.Drawing.Size(23, 22);
+            this.Button_Semantico.Text = "toolStripButton6";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // Button_Salir
+            // 
+            this.Button_Salir.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.Button_Salir.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.Button_Salir.Image = ((System.Drawing.Image)(resources.GetObject("Button_Salir.Image")));
+            this.Button_Salir.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Button_Salir.Name = "Button_Salir";
+            this.Button_Salir.Size = new System.Drawing.Size(23, 22);
+            this.Button_Salir.Text = "toolStripButton7";
+            this.Button_Salir.Click += new System.EventHandler(this.Button_Salir_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.Codigo);
+            this.tabControl1.Location = new System.Drawing.Point(0, 53);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(827, 311);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // Codigo
+            // 
+            this.Codigo.Controls.Add(this.Panel_Codigo);
+            this.Codigo.Location = new System.Drawing.Point(4, 22);
+            this.Codigo.Name = "Codigo";
+            this.Codigo.Padding = new System.Windows.Forms.Padding(3);
+            this.Codigo.Size = new System.Drawing.Size(819, 285);
+            this.Codigo.TabIndex = 0;
+            this.Codigo.Text = "Codigo";
+            this.Codigo.UseVisualStyleBackColor = true;
+            // 
+            // Panel_Codigo
+            // 
+            this.Panel_Codigo.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Panel_Codigo.Location = new System.Drawing.Point(-4, 0);
+            this.Panel_Codigo.Name = "Panel_Codigo";
+            this.Panel_Codigo.Size = new System.Drawing.Size(823, 285);
+            this.Panel_Codigo.TabIndex = 0;
+            this.Panel_Codigo.Text = "";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(4, 366);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(819, 166);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1111, 544);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Nimbus_369";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.Codigo.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem principalToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nuevoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem guardarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem guardarComoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton Button_Abrir;
+        private System.Windows.Forms.ToolStripButton Button_Nuevo;
+        private System.Windows.Forms.ToolStripButton Button_Guardar;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton Button_Lexico;
+        private System.Windows.Forms.ToolStripButton Button_Sintactico;
+        private System.Windows.Forms.ToolStripButton Button_Semantico;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripButton Button_Salir;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage Codigo;
+        private System.Windows.Forms.RichTextBox Panel_Codigo;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Nimbus/Nimbus_01P/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nimbus_01P
+{
+    public partial class Form1 : Form
+    {
+        int cantLineas = 0;
+        string Nombre_Archivo, direccion;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Codigo Agregado
+            tabControl1.Visible = false;
+        }
+
+        #region Metodo_Leer_Archivo
+        public void Leer_archivo(string NombreArchivo)
+        {
+            try
+            {
+                StreamReader Leer = new StreamReader(NombreArchivo, System.Text.Encoding.Default);
+                string texto;
+                texto = Leer.ReadToEnd();
+                Leer.Close();
+                Panel_Codigo.Text = texto;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("No se puede leer el archivo Correctamente, o el archivo esta danado");
+            }
+
+        }
+        #endregion
+
+        #region Metodos_Guardar_GuardaComo_Abrir_Nuevo
+        public void Abrir_archivo()
+        {
+            try
+            {
+                OpenFileDialog ofd = new OpenFileDialog();
+                ofd.Title = "Nimbus_369";
+                ofd.ShowDialog();
+                if (File.Exists(ofd.FileName))
+                {
+                    using (Stream cadena = ofd.OpenFile())
+                    {
+                        Leer_archivo(ofd.FileName);
+                        Nombre_Archivo = ofd.FileName;
+                        direccion = ofd.FileName;
+                        tabControl1.Visible = true;
+
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("El archivo no se abrio correctamente");
+            }
+
+        }
+
+        public void Guardar_Archivo_Como()
+        {
+            try
+            {
+                SaveFileDialog Salvar_Archivo = new SaveFileDialog();
+                Salvar_Archivo.Filter = "Archivos n369|*.n369";
+                if (Salvar_Archivo.ShowDialog() == DialogResult.OK)
+                {
+                    if (File.Exists(Salvar_Archivo.FileName))
+                    {
+                        //Archivo Si existe
+                        StreamWriter codigonuevo = File.CreateText(Salvar_Archivo.FileName);
+                        codigonuevo.Write(Panel_Codigo.Text);
+                        codigonuevo.Flush();
+                        codigonuevo.Close();
+                        Nombre_Archivo = Salvar_Archivo.FileName;
+                        direccion = Salvar_Archivo.FileName;
+                    }
+                    else
+                    {
+                        //Archivo no existe
+                        StreamWriter codigonuevo = File.CreateText(Salvar_Archivo.FileName);
+                        codigonuevo.Write(Panel_Codigo.Text);
+                        codigonuevo.Flush();
+                        codigonuevo.Close();
+                        Nombre_Archivo = Salvar_Archivo.FileName;
+                        direccion = Salvar_Archivo.FileName;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error al guardar archivo");
+            }
+        }
+
+        public void Archivo_Nuevo()
+        {
+            try
+            {
+                tabControl1.Visible = true;
+                Panel_Codigo.Clear();
+                Panel_Codigo.Text = "< !_Nimbus_369_Code_!> \n { \n\t # Nimbus_Main#() \n\t [ \n\n\t ] \n }";
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error al crear archivo");
+            }
+        }
+
+        public void Guardar_Archivo()
+        {
+            try
+            {
+                if (Nombre_Archivo == null)
+                {
+                    Guardar_Archivo_Como();
+
+                }
+                else
+                {
+                    // el archivo nuevo
+                    StreamWriter codigonuevo = File.CreateText(Nombre_Archivo);
+                    codigonuevo.Write(Panel_Codigo.Text);
+                    codigonuevo.Flush();
+                    codigonuevo.Close();
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error al guardar archivo");
+            }
+
+        }
+        #endregion
+
+        #region ToolStrip_Menu_Principal
+        private void abrirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Abrir_archivo();
+        }
+
+        private void nuevoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Archivo_Nuevo();
+        }
+
+        private void guardarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Guardar_Archivo();
+        }
+
+        private void guardarComoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Guardar_Archivo_Como();
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+        #endregion
+
+        #region Botones_Abrir_Salir_Guardar_Nuevo
+        private void Button_Abrir_Click(object sender, EventArgs e)
+        {
+            Abrir_archivo();
+        }
+
+        private void Button_Nuevo_Click(object sender, EventArgs e)
+        {
+            Archivo_Nuevo();
+        }
+
+        private void Button_Guardar_Click(object sender, EventArgs e)
+        {
+            Guardar_Archivo();
+        }
+
+        private void Button_Salir_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+        #endregion
+    }
+}
